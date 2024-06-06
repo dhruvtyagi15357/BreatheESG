4662da700d03a7eefe9b23d96a6624d612149152
Dhruv Tyagi <dhruvtyagi01@gmail.com>
2024-06-07 05:15:58 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import path from 'path';
-import http from 'http';
-
+const express = require('express');
+const path = require('path');
 const app = express();
-const server = http.createServer(app);
 
-dotenv.config();
-const PORT = process.env.PORT || 5000;
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use(express.json());
-server.listen(PORT, async () => {
-    // await connect()
-    console.log(`server listening on the port: ${PORT}`);
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
